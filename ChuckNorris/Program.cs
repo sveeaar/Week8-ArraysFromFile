8e0a749021eb55c0c08afc204ce2ede5418871ca
Sander Veeäär <sveeaar@tktk.ee>
2022-04-02 21:54:48 +0300

    New projects - arrays from file

---
+++ ChuckNorris/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace ChuckNorris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string randomChuck;
+            randomChuck = GetRandomFromFile("chuck.txt");
+
+            Console.WriteLine($"{randomChuck}");
+        }
+
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            return randomIndex;
+        }
+        private static string GetRandomFromFile(string fileName)
+        {
+            string filePath = $@"C:\Users\...\Samples\{fileName}";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            string randomElement = dataFromFile[GenerateRandomIndex(dataFromFile)];
+
+            return randomElement;
+        }
+    }
+}
